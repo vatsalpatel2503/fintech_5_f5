92aa090f72d74075d9241d7fffb479cbdb971469
vatsalpatel2503 <vatsalpansuriya25@gmail.com>
2023-06-28 08:19:06 +0530

    First commit after learning multiple branches

---
+++ PYTHONFILE.py
@@ -0,0 +1,9 @@
+# Input
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+# Output
+result = 42
+print("The result is:", result)
+
+print("we are testing how to get back data from git")
